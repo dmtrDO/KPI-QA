c93b68114b9f6759db889819cb7825d6352e484c
dmtrDO <dmytro.domaratskiy@gmail.com>
2025-10-20 07:44:33 +0300

    add teacher page

--- fcatalog/main/urls.py
+++ fcatalog/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("teacher", views.teacher, name="teacher"),
 ]
--- fcatalog/main/views.py
+++ fcatalog/main/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
+
+
+def teacher(request):
+    return render(request, "teacher.html")
