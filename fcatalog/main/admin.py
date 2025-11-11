109aeba44176e3fd66abc75098f6ffcc812629c7
dmtrDO <dmytro.domaratskiy@gmail.com>
2025-11-11 16:01:17 +0200

    test admin

--- fcatalog/main/admin.py
+++ fcatalog/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Teacher, Discipline, RequestedDiscipline
 
+
 admin.site.register(Teacher)
 admin.site.register(Discipline)
 admin.site.register(RequestedDiscipline)
