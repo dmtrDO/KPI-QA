6b14f23e7611a89e062ef364f6eed268be8ee2e4
dmtrDO <dmytro.domaratskiy@gmail.com>
2025-12-11 13:43:27 +0200

    test_pdf_download

--- fcatalog/selenium/selenium_tests.py
+++ fcatalog/selenium/selenium_tests.py
@@ -6,51 +6,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Запуск Firefox
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 driver.get("https://librwebapp.pythonanywhere.com/")
 
 try:
-    # === ТЕСТ 1: Локатори (пункт а) ===
     # 1. Локатор ID (поле вводу)
     driver.find_element(By.ID, "input_search").send_keys("Асинхронність")
-
     time.sleep(2)
-
-    # 2. Локатор XPath (обов'язково) - кнопка пошуку
     driver.find_element(By.XPATH, "//button[@id='search']").click()
-
     time.sleep(2)
-
-    # 3. Локатор CSS Selector - посилання на логін
     login_link = driver.find_element(By.CSS_SELECTOR, "div.login a")
+    print("Елементи знайдено")
 
-    print("Test 1 OK: Елементи знайдено трьома способами.")
-
-    # === ТЕСТ 2: Очікування (пункт в) ===
     login_link.click()
 
-    # Explicit Wait: чекаємо 5 сек, поки з'явиться заголовок "Авторизація"
     wait = WebDriverWait(driver, 5)
     auth_label = wait.until(EC.visibility_of_element_located((By.ID, "text_auth")))
 
-    print("Test 2 OK: Очікування спрацювало, сторінка завантажилась.")
+    print("Сторінка завантажилась.")
 
-    # === ТЕСТ 3: Перевірки/Asserts (пункт б) ===
-    # Перевірка 1: Заголовок вкладки
-    assert driver.title == "Login", "Помилка Title"
-
+    assert driver.title == "Logind", "Помилка Title"
     time.sleep(1)
-
-    # Перевірка 2: URL адреса
     assert "login" in driver.current_url, "Помилка URL"
-
     time.sleep(1)
-
-    # Перевірка 3: Текст на сторінці
     assert auth_label.text == "Авторизація", "Помилка тексту"
-
-    print("Test 3 OK: Всі перевірки пройшли успішно.")
+    print("Перевірки пройшли успішно.")
 
 except Exception as e:
     print(f"Помилка: {e}")
